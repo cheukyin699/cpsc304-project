8e293c9ebaabebdfe16abe279f66c3a89a3b6423
EmiliaChen <emiliac0717@gmail.com>
2020-03-28 12:58:15 -0700

    added model objects to medRecord, diag test, pateient

--- src/main/java/org/cpsc304/bigdata/model/MedicalInfo/diagnosticTest.java
+++ src/main/java/org/cpsc304/bigdata/model/MedicalInfo/diagnosticTest.java
@@ -1,4 +1,18 @@
 package org.cpsc304.bigdata.model.MedicalInfo;
 
 public class diagnosticTest {
+
+    private String testName;
+    private int accuracy;
+
+    public diagnosticTest(String testName, int accuracy) {
+        this.testName = testName;
+        this.accuracy = accuracy;
+    }
+
+    public String getTestName() { return testName; }
+    public int getAccuracy() { return accuracy; }
+
+    public void setTestName(String testName) { this.testName = testName; }
+    public void setAccuracy(int accuracy) {this.accuracy = accuracy; }
 }
--- src/main/java/org/cpsc304/bigdata/model/People/patient.java
+++ src/main/java/org/cpsc304/bigdata/model/People/patient.java
@@ -1,4 +1,37 @@
 package org.cpsc304.bigdata.model.People;
 
 public class patient {
+    private String id;
+    private String patientName;
+    private String address;
+    private String familyHistory;
+    private int age;
+    private int sex;
+    private String physicianName;
+
+    public patient(String id, String name, String address, String familyHistory, int age, int sex, String physicianName) {
+        this.id = id;
+        this.patientName = name;
+        this.address = address;
+        this.familyHistory = familyHistory;
+        this.age = age;
+        this.sex = sex;
+        this.physicianName = physicianName;
+    }
+
+    public String getId() { return id; }
+    public String getpatientName() {return patientName; }
+    public String getAddress() {return address; }
+    public String getFamilyHistory() {return familyHistory; }
+    public int getAge() {return age; }
+    public int getSex() { return sex; }
+    public String getPhysicianName() {return physicianName; }
+
+    public void setId(String id) {this.id = id; }
+    public void setPatientName(String patientName) {this.patientName = patientName; }
+    public void setAddress(String address) {this.address = address; }
+    public void setFamilyHistory(String familyHistory) {this.familyHistory = familyHistory; }
+    public void setAge(int age) {this.age = age; }
+    public void setSex(int sex) {this.sex = sex; }
+    public void setPhysicianName(String physicianName) { this.physicianName = physicianName; }
 }
